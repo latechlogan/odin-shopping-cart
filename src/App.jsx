7f0345efe707f1c3bbc904f355c1c71240c43027
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-12-03 16:36:26 -0600

    refactor: move routing from main to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./routes/Home.jsx";
+import Products from "./routes/Products.jsx";
+import ProductDetails from "./routes/ProductDetails.jsx";
+import Cart from "./routes/Cart.jsx";
 
-function App() {
-  return <h1>App works!</h1>;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route
+          path="/product-details/:productId"
+          element={<ProductDetails />}
+        />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
-import Products from "./routes/Products.jsx";
-import ProductDetails from "./routes/ProductDetails.jsx";
-import Cart from "./routes/Cart.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/products" element={<Products />} />
-        <Route
-          path="/product-details/:productId"
-          element={<ProductDetails />}
-        />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
