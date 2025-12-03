f68266d5b226b95a54a57ff78821275ab6a2f218
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-12-03 16:48:31 -0600

    feat: fetch and store product data in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,33 @@ import Home from "./routes/Home.jsx";
 import Products from "./routes/Products.jsx";
 import ProductDetails from "./routes/ProductDetails.jsx";
 import Cart from "./routes/Cart.jsx";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+        console.log(jsonData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
