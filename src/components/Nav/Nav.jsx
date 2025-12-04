1ac0947608c8cfbb273f005802f50e8694a39d60
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-12-04 15:53:52 -0600

    style: add basic nav layout and styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,27 @@
 import styles from "./Nav.module.css";
 import { NavLink } from "react-router";
+import { ShoppingCart } from "react-feather";
 
 export default function Nav() {
   return (
-    <navbar>
-      <h1>Odin Shopping Cart</h1>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/products">Products</NavLink>
-        <NavLink to="/product-details/1">Product Details</NavLink>
-        <NavLink to="/cart">Cart</NavLink>
-      </nav>
-    </navbar>
+    <section className={styles.sectionWrapper}>
+      <div className={styles.navbar}>
+        <h1 className={styles.logo}>Odin Shopping Cart</h1>
+        <nav className={styles.navContainer}>
+          <NavLink to="/" className={styles.navLink}>
+            Home
+          </NavLink>
+          <NavLink to="/products" className={styles.navLink}>
+            Products
+          </NavLink>
+          <NavLink to="/product-details/1" className={styles.navLink}>
+            Product Details
+          </NavLink>
+          <NavLink to="/cart" className={styles.navLink}>
+            <ShoppingCart />
+          </NavLink>
+        </nav>
+      </div>
+    </section>
   );
 }
