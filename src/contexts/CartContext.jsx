a07c3cc9d14fa689fcb9bb43bca7a7fd89892995
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-12-03 23:30:06 -0600

    feat: implement addToCart method in CartProvider

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,12 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useMemo } from "react";
 
 const CartContext = createContext();
 
 function CartProvider({ children }) {
   const [cart, setCart] = useState({});
-  const value = { cart, setCart };
 
-  return <CartContext value={value}>{children}</CartContext>;
+  const addToCart = (product, quantity = 1) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart[product.id];
+
+      return {
+        ...prevCart,
+        [product.id]: {
+          ...product,
+          quantity: existingItem ? existingItem.quantity + quantity : quantity,
+        },
+      };
+    });
+  };
+
+  //   const value = { cart, setCart, addToCart };
+  const value = useMemo(() => ({ cart, setCart, addToCart }), [cart]);
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 }
 
 export { CartContext, CartProvider };
