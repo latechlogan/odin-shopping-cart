f6c8c83ec8a2a4978721a70532a165e5cf3c8f4e
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-12-10 15:35:29 -0600

    feat: display products with details in Cart

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,3 +1,55 @@
+import styles from "./Cart.module.css";
+import { useCart } from "../contexts/CartContext";
+import { useState } from "react";
+
 export default function Cart() {
-  return <h2>Cart works!</h2>;
+  const { cart } = useCart();
+  const cartArray = Object.values(cart);
+
+  if (Object.keys(cart).length === 0)
+    return (
+      <div className={styles.emptyError}>
+        <h2>Your cart appears to be empty.</h2>
+      </div>
+    );
+
+  return (
+    <div className={styles.sectionWrapper}>
+      <div className={styles.cart}>
+        <div className={styles.products}>
+          {cartArray.map((item) => {
+            return <CartItem item={item} />;
+          })}
+        </div>
+        <div className={styles.charges}></div>
+      </div>
+    </div>
+  );
+}
+
+export function CartItem({ item }) {
+  return (
+    <div key={item.id} className={styles.cartItem}>
+      <div className={styles.itemImgContainer}>
+        <img
+          src={item.image}
+          alt={item.description}
+          className={styles.itemImg}
+        />
+      </div>
+      <div className={styles.itemDetails}>
+        <span className={styles.itemTitle}>{item.title}</span>
+        <span className={styles.itemPrice}>${item.price.toFixed(2)}</span>
+        <span className={styles.itemQty}>
+          <UpdateCartQuantity item={item} />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export function UpdateCartQuantity({ item }) {
+  const [quantity, setQuantity] = useState(item.quantity);
+
+  return <span>{quantity}</span>;
 }
