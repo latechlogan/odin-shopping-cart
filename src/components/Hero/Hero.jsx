feda15eb06e7ca333c3bc53e2a4d39b5864f865d
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-12-11 15:06:06 -0600

    style: further refine styling of hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,8 +5,17 @@ export default function Hero() {
   return (
     <section className={styles.sectionWrapper}>
       <div className={styles.heroSection}>
+        <span className={styles.photoCred}>
+          Photo by{" "}
+          <a href="https://unsplash.com/@marjantaghipour?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Marjan Taghipour
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/photos/a-woman-in-a-red-dress-is-holding-a-green-umbrella-W_iBXR2m6cw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </span>
         <div className={styles.heroText}>
-          <span className={styles.eyebrow}>New Arrivals</span>
           <h2 className={styles.headline}>
             Elevated Essentials for Every Occasion
           </h2>
